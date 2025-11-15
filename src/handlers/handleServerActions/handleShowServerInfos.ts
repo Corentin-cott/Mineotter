01cd5d48968889003620a52370514e7a4d1de33e
Corentin COTTEREAU <109530954+Corentin-cott@users.noreply.github.com>
2025-11-15 01:59:49 +0100

    - Installation d'Otterbots - Configuration du .env.exemple avec les paramètre non confidentielles par défaut

--- src/handlers/handleServerActions/handleShowServerInfos.ts
+++
@@ -1,9 +0,0 @@
-import { StringSelectMenuInteraction } from "discord.js";
-import buildServerEmbed from "../../utils/buildServerEmbed";
-import { ServeurType } from "../../types/otterly";
-
-export default async function showServerInfo(interaction: StringSelectMenuInteraction, serveur: ServeurType) {
-    return interaction.editReply({
-        embeds: [buildServerEmbed(interaction, serveur, "infos")],
-    });
-}
